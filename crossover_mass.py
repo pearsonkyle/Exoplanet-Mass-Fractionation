5057b3d2de9540f1706b96fb9af9bdf0986494da
Kyle Pearson <pearsonk@email.arizona.edu>
2018-04-26 19:59:33 -0700

    added transcendental equation to solve for crossover mass

--- crossover_mass.py
+++ crossover_mass.py
@@ -20,13 +20,24 @@ mearth = 5.97e28 # g
 AU = 1.496e+13 # cm 
 pc = 3.086e+18 # cm 
 
+# load in atomic data for kinetic radius  
+datatype = [
+    ('atomic number','i8'),
+    ('atom','U3'),
+    ('mass','f8'),
+    ('electronegativity','f8'),
+    ('radius','f8'),
+    ('radius error','f8')
+]
+atom_data = np.genfromtxt('atomic van der waal radii.csv',delimiter=',',dtype=datatype,skip_header=1)
+
 class AttributeDict(dict):
     def __getattr__(self, name):
         return self[name]
     def __setattr__(self,name,val):
         self[name] = val
 
-class grid(object):
+class Interpolator(object):
     def __init__(self,data):
         self.data = np.array(data)
         self.fn = interp1d(self.data[:,0],self.data[:,1],assume_sorted=False,kind='linear',fill_value='extrapolate')
@@ -43,9 +54,9 @@ def Q(d1,d2):
     # d1,d2 collision diameters
     return (np.pi/16)*(d1+d2)**2
 
-def binaryparam(T,m1,m2,d1,d2):
+def binarydiffusion(T,m1,m2,d1,d2):
     '''
-    Hard-sphere approximation for binary diffusion coefficient 
+    Hard-sphere approximation for binary diffusion coefficient [1/cm/s]
 
     input parameters: 
         T - Temperature [k]
@@ -56,15 +67,25 @@ def binaryparam(T,m1,m2,d1,d2):
     b = 3./(64*q) * ( 2*np.pi*kb*T*(m1+m2)/(m1*m2) )**0.5
     return b
 
-def crossover_eval(m2,*args):
-    Feuv,R,M,T,x1 = args 
-    b1 = binaryparam(T,m1,m2,d1,d2)
-    value = m1 + 0.25*Feuv*(R**3)*kb*T / ( (G*M)**2 *m1 *b1 *x1 ) - m2
-    return value 
+kinetic_radius = Interpolator( np.array([atom_data['mass'],atom_data['radius']]).T )
 
-# 2 grids, one more crossover mass, the other for collision diameters
-# def photorate(opt_ni, *args) 
-#findzero( crossover_eval, 0, 40, args=(pars,) )
+def crossover_zero(m2,*args):
+    '''
+        Transcendtal form of cross over mass with 
+        variable diffusion coefficient that depends on
+        the cross over mass
+
+        input parameters:
+            m2 - crossover mass [amu]    
+    '''
+    Feuv,R,M,T,m1,x1 = args 
+
+    # get the van der waals radius for mass of atom
+    d1 = 2*kinetic_radius(m1)*1e-8 # convert Angstrom to cm
+    d2 = 2*kinetic_radius(m2*mp)*1e-8 
+    b1 = binarydiffusion(T,m1,m2,d1,d2)
+
+    return crossover(*args,b1)/mp - m2
 
 def massloss(Feuv,m1,M,R):
     '''
@@ -73,12 +94,13 @@ def massloss(Feuv,m1,M,R):
         m1 - mass of constituent 1 being carried away [g]
         M - mass of planet [g]
         R - radius of planet [cm]
+        returns [g/s]?
     '''
     gp = G*M/R # grav potential
     # pi r^2 being absorbed but 4*pi^2 being emitted 
     return (1./4) * Feuv * 1./(gp*m1)
 
-def crossover(Feuv,R,M,T,m1,b1,x1):
+def crossover(Feuv,R,M,T,m1,x1,b1):
     '''
     Crossover mass at which a heavier second constituent will not be dragged along with constituent 1
     See Hunten 1987 
@@ -96,45 +118,35 @@ def crossover(Feuv,R,M,T,m1,b1,x1):
     return mass
 
 if __name__ == "__main__":
-
     
     # parameters for planet 
     pars = AttributeDict({
-        'radius':0.3767*rjup, # cm
-        'distance':0.02872, # AU
-        'mass':0.0727*mjup, # g
-        'm1':1*mp, # g
-        'starspec':'hlsp_muscles_multi_multi_gj436_broadband_v22_const-res-sed.fits',
-        'b1':lambda T:  1.04*10**18 * T**0.732, 
-        'X1':0.9,
-        'T':600, 
+        'R':0.3767*rjup, # Radius of Planet [cm]
+        'M':0.0727*mjup, # Mass of Planet [g]
+        'a':0.0386*AU, # Semi-major Axis [AU]
+        'm1':1*mp, # Mass of Constituent 1 [g]
+        'starspec':'Spectra/hlsp_muscles_multi_multi_gj436_broadband_v22_const-res-sed.fits',
+        'b1':lambda T:  1.04*10**18 * T**0.732, # binary diffusion coefficient [1/cm/s]
+        'x1':0.9, # Fraction of Constituent 1 
+        'T':600, # Temperature of Planet [K]
+        'distance':10.14 # Distance to planet [parsec]
     })
+
+    # H-He diffusion coefficients are different by a factor of 3
+    # the paramterized function is larger
 
     # load data from star into grid 
     spec = fits.getdata(pars.starspec,1)
-    mask = spec['WAVELENGTH'] < 912
-    Feuv = spec['FLUX'][mask] * spec['WAVELENGTH'][mask] * 1e-8 / (h*c) / pars.distance**2
+    mask = spec['WAVELENGTH'] < 912 # EUV wavelength
+    Feuv = spec['FLUX'][mask] * (pars.distance*pc)**2/pars.a**2  # * spec['WAVELENGTH'][mask] * 1e-8 / (h*c)
     pars.Feuv = trapz(Feuv,spec['WAVELENGTH'][mask])
     pars.FLUX = spec['FLUX'][mask]
     pars.WAVELENGTH = spec['WAVELENGTH'][mask]
 
-    pars.g = G*pars.mass/pars.radius**2
-    
-    pars.escape = escape(pars.Feuv,
-                         pars.m1,
-                         pars.mass,
-                         pars.radius)
-
-    pars.crossover = crossover(pars.m1,
-                                pars.escape,
-                                pars.T,
-                                pars.b1(pars.T),
-                                pars.g,
-                                pars.X1)
-
-    f,ax = plt.subplots(1)
-    ax.plot(spec['WAVELENGTH'][mask],spec['FLUX'][mask])
-    ax.set_xlabel('Wavelength (Angstroms)')
-    ax.set_ylabel('Flux Density (erg/cm2/s/Ang)')
-    plt.show()
-    
+    cmass = findzero(crossover_zero, 1, 100, args=(pars.Feuv,
+                                                    pars.R,
+                                                    pars.M,
+                                                    pars.T,
+                                                    pars.m1,
+                                                    pars.x1) )
+    print('crossover mass: {:.1f} amu'.format(cmass) )    
--- interactive.py
+++ interactive.py
@@ -1,4 +1,4 @@
-from escape import *
+from crossover_mass import *
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 import matplotlib.colors as colors
@@ -9,9 +9,9 @@ pars = AttributeDict({
         'M':0.0727*mjup, # Mass of Planet [g]
         'a':0.0386*AU, # Semi-major Axis [AU]
         'm1':1*mp, # Mass of Constituent 1 [g]
-        'starspec':'hlsp_muscles_multi_multi_gj436_broadband_v22_const-res-sed.fits',
+        'starspec':'Spectra/hlsp_muscles_multi_multi_gj436_broadband_v22_const-res-sed.fits',
         'b1':lambda T:  1.04*10**18 * T**0.732, # binary diffusion coefficient [1/cm/s]
-        'X1':0.9, # Fraction of Constituent 1 
+        'x1':0.9, # Fraction of Constituent 1 
         'T':600, # Temperature of Planet [K]
         'distance':10.14 # Distance to planet [parsec]
          })
@@ -24,29 +24,40 @@ pars.Feuv = trapz(Feuv,spec['WAVELENGTH'][mask])
 pars.FLUX = spec['FLUX'][mask]
 pars.WAVELENGTH = spec['WAVELENGTH'][mask]
 
-#f,axs = plt.subplots(1)
-#axs.plot(spec['WAVELENGTH'][mask],Feuv)
-#axs.set_xlabel('Wavelength (Angstroms)')
-#axs.set_ylabel('Flux Density (erg/cm2/s/Ang)')
-#plt.show()
+f,axf = plt.subplots(1)
+axf.plot(spec['WAVELENGTH'], spec['FLUX'],'k-')
+axf.plot(spec['WAVELENGTH'][mask],Feuv,'r-')
+axf.set_xlabel('Wavelength (Angstroms)')
+axf.set_ylabel('Flux Density (erg/cm2/s/Ang)')
+
 
 M = np.linspace( 0.5, 4, 90)*mearth
 R = np.linspace( 0.5, 6, 40)*rearth
 mg, rg = np.meshgrid(M,R)
 
 T=500
-cgrid = crossover(pars.Feuv,
-                        rg, mg,
-                        pars.T,
-                        pars.m1,
-                        pars.b1(pars.T),
-                        pars.X1)
 
-mask = (cgrid/mp) > 28
-cgrid[mask] = 29*mp
+# TODO TEST THIS
+for i in range(R.shape[0]):
+    for j in range(M.shape[0]):
+                
+        try:
+                cmass = findzero(crossover_zero, 1, 10000, args=(pars.Feuv,
+                                                        R[i],
+                                                        M[j],
+                                                        pars.T,
+                                                        pars.m1,
+                                                        pars.x1) )
+                mg[i,j] = cmass
+        except:
+                import pdb; pdb.set_trace()
+
+                
+mask = mg > 100
+mg[mask] = 101
 
 f,axg = plt.subplots(1)
-cp = axg.contourf( M/mearth, R/rearth, (cgrid/mp), 40,cmap='nipy_spectral',vmin=1,vmax=28 )
+cp = axg.contourf( M/mearth, R/rearth, mg, 40,cmap='nipy_spectral',vmin=1,vmax=100 )
 axg.plot( pars.M/mearth, pars.R/rearth, 'ko')
 axg.set_ylabel('Radius (Earth)')
 axg.set_xlabel("Mass (Earth)")
